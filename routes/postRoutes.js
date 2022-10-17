077fcbe96c8a2065d7290ad396f94bf682cb3546
Oubay chetouani <oubaygamer@gmail.com>
2022-10-17 22:40:49 +0100

    fs add controller

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const controller = require("../controllers/");
+const controller = require("../controllers/postController");
 
 const router = express.Router();
 router.route("/").get(controller.getAllUsers).post(controller.createUser);
