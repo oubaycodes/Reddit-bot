777c65ff2d6a40475ea778cb31c05a8df210fe55
Oubay chetouani <oubaygamer@gmail.com>
2022-10-17 21:44:32 +0100

    MVC filesystem implementaion

---
+++ app.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const morgan = require("morgan");
+
+// create app
+const app = express();
---
+++ server.js
@@ -0,0 +1,5 @@
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
+// running app
+const app = require("./app");
